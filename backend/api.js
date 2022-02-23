fa2b1f02f7c531643edb7ed0b201890603e77973
manishnepali <manish.nepali@student.ehb.be>
2022-02-23 16:23:17 +0100

    get timesheet approvals

--- backend/api.js
+++ backend/api.js
@@ -1,10 +1,11 @@
 
 import express, { response, text } from 'express';
 import fetch from 'node-fetch';
-
+import cors from 'cors';
 
 const app = express()
 const port = 3080
+app.use(cors());
 
 const test = "http://{JIRA_BASE_URL}/rest/tempo-timesheets/4/timesheet-approval/pending http://{JIRA_BASE_URL}/rest/tempo-timesheets/4/worklogs/{worklogId}"
 //feych users from abano 
@@ -49,7 +50,7 @@ const test = "http://{JIRA_BASE_URL}/rest/tempo-timesheets/4/timesheet-approval/
   .catch(err => console.error(err));
 
 //fetch all projeects from abano
-  await fetch('https://abano-playground.atlassian.net/rest/api/3/user/search?query=a', {
+  await fetch('https://abano-playground.atlassian.net/rest/api/3/project', {
     method: 'GET',
     headers: {
       'Authorization': `Basic ${Buffer.from(
@@ -74,3 +75,42 @@ const test = "http://{JIRA_BASE_URL}/rest/tempo-timesheets/4/timesheet-approval/
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+await fetch('https://api.tempo.io/core/3/teams', {
+  method: 'GET',
+  headers: {
+    'Authorization': "Bearer s0sHctOaYXAtK2bmSI4tVj9NrtOxiS",
+    'Accept': 'application/json'
+  }
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    )
+    return response.json();
+  })
+  .then(text => app.get('/team', (req, res) => {
+    res.send(text)
+  }))
+  .catch(err => console.error(err));
+
+ 
+  
+await fetch(`https://api.tempo.io/core/3/timesheet-approvals/user/61bb432957d5c3007124f6d5`, {
+  method: 'GET',
+  headers: {
+    'Authorization': "Bearer s0sHctOaYXAtK2bmSI4tVj9NrtOxiS",
+    'Accept': 'application/json'
+  }
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    )
+    return response.json();
+  })
+  .then(text => app.get('/approved', (req, res) => {
+    res.send(text)
+  }))
+  .catch(err => console.error(err));
